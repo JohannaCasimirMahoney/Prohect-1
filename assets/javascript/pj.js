7e9e374a8e8e5a1467e3a54505abd63d777a438e
Anna rozymuradov <annarozymuradov@campus-075-022.ucdavis.edu>
2019-04-25 20:55:24 -0700

    beginning

---
+++ assets/javascript/pj.js
@@ -0,0 +1,13 @@
+ $(document).ready(function() {
+ 
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAI2dDwoRrxUVA-gtYKr6I0HnyLRZh5HCQ",
+    authDomain: "prohect-7f228.firebaseapp.com",
+    databaseURL: "https://prohect-7f228.firebaseio.com",
+    projectId: "prohect-7f228",
+    storageBucket: "prohect-7f228.appspot.com",
+    messagingSenderId: "790901056453"
+  };
+  firebase.initializeApp(config);
+});
