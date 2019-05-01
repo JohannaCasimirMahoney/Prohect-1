5cdbfab037f850cfd2d27127781b283a6c9a8cc7
Ghost <ghost@campus-093-013.ucdavis.edu>
2019-04-30 20:42:21 -0700

    updated search bar

--- assets/javascript/pj.js
+++ assets/javascript/pj.js
@@ -42,12 +42,10 @@ function filterFunction() {
 
 
 // Yelp API
-// "use strict";
-
 var apiKey = "Mmic2d8wVdogrtxq2vDSeF44D35XjDRePYKnTd3DoYFJZgzxFP5eJKCwrUxMu8D53RSohe8VfuvnVvj9jSo30AUTo-KsSJnw4bG6MubT0HfshOsRUxymYSunnIXCXHYx";
 
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-var queryUrl = "https://api.yelp.com/v3/events?location=" + "Sacramento";
+var queryUrl = "https://api.yelp.com/v3/businesses/search?location=" + "Sacramento"
 // var place = $("#zipcode").val();
 // console.log(place);
 // Make the Yelp Fusion API call
@@ -58,20 +56,62 @@ $.ajax({
 
   url: proxyUrl + queryUrl,
   method: "GET",
-  
+
 }).then(function (response) {
   console.log(response);
 
-  console.log(response.events["0"].event_site_url);
+  console.log(response.events["0"].businesses_site_url);
   console.log(response.events["0"].image_url);
   console.log(response.events["0"].name);
   console.log(response.events["0"].description);
 
 });
-// Creating an AJAX call for the specific giphy button being clicked
-// $.ajax({
-//   url: proxyUrl + queryUrl,
-//   method: "GET"
-// }).then(function (response) {
-//   console.log(response);
-// });
+
+// Yelp responisve (search)
+function searchmechanicInTown(business) {
+  var apiKey = "Mmic2d8wVdogrtxq2vDSeF44D35XjDRePYKnTd3DoYFJZgzxFP5eJKCwrUxMu8D53RSohe8VfuvnVvj9jSo30AUTo-KsSJnw4bG6MubT0HfshOsRUxymYSunnIXCXHYx";
+
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  var queryUrl = "https://api.yelp.com/v3/businesses/search?location=" + "Sacramento" + "&term=" + business;
+  // var place = $("#zipcode").val();
+  // console.log(place);
+  // Make the Yelp Fusion API call
+  $.ajax({
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer " + apiKey)
+    },
+
+    url: proxyUrl + queryUrl,
+    method: "GET",
+
+  }).then(function (response) {
+
+    // Printing the entire object to console
+    console.log(response);
+
+    // Constructing HTML containing the artist information
+    var businessName = $("<h1>").text(response.name);
+    var businessURL = $("<a>").attr("href", response.url).append(businessName);
+    var businessImage = $("<img>").attr("src", response.thumb_url);
+
+    // Empty the contents of the artist-div, append the new artist content
+    $("#cars").empty();
+    $("#cars").append(businessURL, businessImage);
+  });
+}
+
+// Event handler for user clicking the select-artist button
+$(".search-btn").on("click", function (event) {
+
+  // Preventing the button from trying to submit the form
+  event.preventDefault();
+  // Storing the artist name
+  var inputBusiness = $("#cars").val().trim();
+
+  // Running the searchBandsInTown function(passing in the artist as an argument)
+  searchmechanicInTown(inputBusiness);
+
+
+});
+
+// function to to call.then functions
